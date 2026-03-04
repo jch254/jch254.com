ccb0fc97122e4e2c3d19562e5405007d3f8a9823
Jordan Hornblow <jordan@jch254.com>
2026-03-05 11:24:29 +1300

    Add SoundCloud, Spotify, Vimeo and YouTube embeds into blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,24 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import AutoImport from 'astro-auto-import';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jch254.com',
-  integrations: [react(), mdx(), tailwind(), sitemap()],
+  integrations: [
+    AutoImport({
+      imports: [
+        './src/components/embeds/YouTube.astro',
+        './src/components/embeds/SoundCloud.astro',
+        './src/components/embeds/Spotify.astro',
+        './src/components/embeds/Vimeo.astro',
+      ],
+    }),
+    react(),
+    mdx(),
+    tailwind(),
+    sitemap(),
+  ],
   output: 'static',
 });
